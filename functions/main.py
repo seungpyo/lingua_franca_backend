25abe91eb5ec26d8bc44fda82d3424690739aefb
Seungpyo Hong <spkbk98@gmail.com>
2023-09-25 21:22:02 +0900

    fetches prompts from firestore

--- functions/main.py
+++ functions/main.py
@@ -3,7 +3,7 @@
 # Deploy with `firebase deploy`
 
 from firebase_functions import https_fn
-from firebase_admin import initialize_app, get_app
+from firebase_admin import initialize_app, get_app, firestore
 from google.cloud.secretmanager import SecretManagerServiceClient
 import openai
 from typing import Any, Dict, List
@@ -13,27 +13,30 @@ from flask import jsonify
 
 initialize_app()
 model = "gpt-3.5-turbo"
-reply_system_prompt = f"""
-You are an English speaking tutor who is replying to a student's messages on random topics.
-Try to reply to the student's messages so that student can learn various English expressions.
-Since you are an educator, please use polite and appropriate language.
-Your main goal is keep the conversation going, and have student be more engaged in the conversation.
-"""
-feedback_system_prompt = """
-You are an English tutor who gives feedback to a student's messages on random topics.
-Try to find out awkward statements, grammar mistakes, or spelling mistakes in the student's messages.
-DO NOT say things other than linguistic feedback. If there is no linguistic feedback, just say things like "Good job!".
-Since you are an educator, please use polite and appropriate language.
-Also, try not to hurt the student's feelings. You may use emojis to soften your feedback.
-Since the student is native {student_language} speaker, you MUST reply in {student_language}.
-DO NOT just translate student's answer; give some educational feedback so that student can learn English.
-"""
+
 cors_headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST",
     "Access-Control-Allow-Headers": "Content-Type",
     "Access-Control-Max-Age": "3600",
 }
+
+def get_propmpt_from_firestore(prompt_collection: str, version: int | None) -> str:
+    db = firestore.client()
+    try:
+        if version is None:
+            docs = db.collection(prompt_collection).order_by("version", direction=firestore.Query.DESCENDING).limit(1).get()
+        else:
+            query = db.collection(prompt_collection).where("version", "==", str(version))
+            docs = query.get()
+    except Exception as e:
+        raise Exception(f"Failed to get prompt for {prompt_collection} with version {version}: {e}")
+    if len(docs) == 0:
+        raise Exception(f"Failed to get prompt for {prompt_collection} with version {version}")
+    version = docs[0].to_dict()["version"] if version is None else version
+    prompt = docs[0].to_dict()["content"]
+    print(f"Got prompt for {prompt_collection} with version {version}: {prompt}")
+    return prompt
 
 @https_fn.on_request()
 def lingua_franca_openai_proxy(req: https_fn.Request) -> https_fn.Response:
@@ -78,10 +81,50 @@ def lingua_franca_openai_proxy(req: https_fn.Request) -> https_fn.Response:
             response=msg, 
             status=400)
     student_language = req_json["student_language"]
+
+    if "last_user_message_id" not in req_json:
+        msg = "Missing \"last_user_message_id\" field in request JSON"
+        print(msg)
+        return https_fn.Response(
+            headers=cors_headers,
+            response=msg, 
+            status=400)
+    last_user_message_id = req_json["last_user_message_id"]
+
     # Message slicing is done by client now.
     previous_messages: List[Dict[str, str]] = messages
+    latest_user_message_candidates = [message for message in previous_messages if message["id"] == last_user_message_id]
+    if len(latest_user_message_candidates) == 0:
+        msg = f"Could not find message with id {last_user_message_id}"
+        print(msg)
+        return https_fn.Response(
+            headers=cors_headers,
+            response=msg, 
+            status=400)
+    latest_user_message = latest_user_message_candidates[0]
+
+    # This loop also removes the "id" field from the `latest_user_message`
+    for message in previous_messages:
+        del message["id"]
 
     print("Creating reply response...")
+    try:
+        reply_system_prompt = get_propmpt_from_firestore("reply-system", None)
+    except Exception as e:
+        return https_fn.Response(
+            headers=cors_headers,
+            response=f"Failed to get reply system prompt: {e}", 
+            status=500,
+        )
+    try:
+        feedback_system_prompt = get_propmpt_from_firestore("feedback-system", None)
+    except Exception as e:
+        return https_fn.Response(
+            headers=cors_headers,
+            response=f"Failed to get feedback system prompt: {e}", 
+            status=500,
+        )
+    
     reply_response = openai.ChatCompletion.create(
         model=model,
         temperature=0.5,
@@ -102,7 +145,7 @@ def lingua_franca_openai_proxy(req: https_fn.Request) -> https_fn.Response:
                 "role": "system", 
                 "content": feedback_system_prompt.format(student_language=student_language),
             },
-            *previous_messages,
+            latest_user_message,
         ],
     )
     def parse_openai_response_dict(response: Dict[str, Any]):
